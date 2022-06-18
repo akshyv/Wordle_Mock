68746a2efd1baa3102398882a7aca3d7bf712d4c
akshyv <akshy.nattu@gmail.com>
2022-06-18 13:37:47 +0530

    corrected the code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
+const wordle = "SUPER";
 
 const keys = [
   "Q",
@@ -39,29 +40,41 @@ const guessRows = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
+let currentRow = 0;
+let currentTile = 0;
 
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement("div");
-  rowElement.setAttribute("id", "guessRow-", +guessRowIndex);
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
   guessRow.forEach((guess, guessIndex) => {
     const tileElement = document.createElement("div");
     tileElement.setAttribute(
       "id",
-      "guessRow",
-      +guessRowIndex + "-tile-" + guessIndex
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
     );
     tileElement.classList.add("tile");
     rowElement.append(tileElement);
   });
+
   tileDisplay.append(rowElement);
 });
-const handleClick = () => {
-  console.log("clicked");
-};
+
 keys.forEach((key) => {
   const buttonElement = document.createElement("button");
   buttonElement.textContent = key;
   buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", handleClick);
+  buttonElement.addEventListener("click", () => handleClick(key));
   keyboard.append(buttonElement);
 });
+
+const handleClick = (key) => {
+  console.log("clicked", key);
+  addLetter(key);
+};
+
+const addLetter = (letter) => {
+  const tile = document.getElementById(
+    "guessRow-" + currentRow + "-tile-" + currentTile
+  );
+  tile.textContent = letter;
+};
